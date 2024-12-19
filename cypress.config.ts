1e3512912aa1879494d514b2d9a72a4812eb82e3
Zinedine Sghari <zinedine.sghari@infosupport.com>
2024-12-19 10:53:18 +0100

    extra dingen

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,15 @@ export default {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl : 'http://localhost:4200',
+    baseUrl: "http://localhost:4200",
     includeShadowDom: true,
+  },
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
   },
 };
--- src/app/authentication.service.spec.ts
+++ src/app/authentication.service.spec.ts
@@ -1,27 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { AuthenticationService } from './authentication.service';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { jwtDecode } from 'jwt-decode';
-import { environment } from '../environments/environment';
+import { RouterTestingModule } from '@angular/router/testing';
 import { Register } from './models/register';
 import { Login } from './models/login';
+import { environment } from '../environments/environment';
 
-describe('AuthenticationService', () => {
-  let service: AuthenticationService;
-  let httpMock: HttpTestingController;
-  let mockRouter: Router;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthenticationService);
-    providers: [ 
-    ]
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+describe('AuthenticationService', () => {});
